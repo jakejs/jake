e53516d39c36d4fed10e9093cc8d842417672c73
zanni bertrand <zanni.bertrand@gmail.com>
2011-05-07 13:15:40 +0200

    add -u utils directory for cross-application script

---
+++ utils/install.js
@@ -0,0 +1,51 @@
+var sys = require('sys');
+
+
+
+desc('Install required npm modules');
+task('default', [], function () {
+	
+	var args = Array.prototype.slice.call(arguments);
+	console.log(args);
+	// Get npm programatically and the fs module
+	var npm = require('/usr/local/lib/node_modules/npm'),
+			fs = require("fs");
+
+	// Load npm
+	npm.load({}, function (error) {
+
+		if (error) {
+			console.log("Cannot load npm");
+			throw error;
+		} else {
+
+			// Read in and parse dependencies file
+			var depends = JSON.parse(fs.readFileSync(args[0]+'/config/npm-depends.json')),
+
+					// Count number of packages to install
+					// because we're going to do it async
+					// and will need to know when the last
+					// one is done, set callback to decrement
+					// count every time a package finishes
+					toInstall = depends.length,
+					afterInstall = function () {
+						toInstall = toInstall - 1;
+						if (toInstall === 0) {
+							// Signify that the task is complete
+							complete();
+						}
+					};
+
+			console.log("Installing " + toInstall + " package(s) and their dependencies...");
+
+			// Install the packages
+			depends.forEach(function (d) {
+				npm.commands.install([d], afterInstall);
+
+			});
+
+		}
+
+	});
+
+}, true);

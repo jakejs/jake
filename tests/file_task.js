c70a7bb554c8d793c061fc0b6422888f5558416e
mde <mde@fleegix.org>
2011-12-16 21:36:54 -0800

    Actual file-task may be async too. Thanks, test.

--- tests/file_task.js
+++ tests/file_task.js
@@ -9,9 +9,11 @@ var tests = new (function () {
   this.testConcatTwoFiles = function () {
     h.exec('../bin/cli.js fileTest:foo/concat.txt', function (out) {
       var data;
-      assert.equal('default task\nfileTest:foo/src2.txt task\n' +
-          'fileTest:foo/concat.txt task\nfileTest:foo/src1.txt task', out);
+      assert.equal('fileTest:foo/src1.txt task\ndefault task\nfileTest:foo/src2.txt task\n' +
+          'fileTest:foo/concat.txt task', out);
+      // Check to see the two files got concat'd
       data = fs.readFileSync(process.cwd() + '/foo/concat.txt');
+      assert.equal('src1src2', data.toString());
       exec('rm -fr ./foo', function (err, stdout, stderr) {
         if (err) { throw err }
         if (stderr || stdout) {

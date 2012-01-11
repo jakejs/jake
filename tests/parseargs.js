c5bc006d825acd7dc815a936ce94eec4d468367c
mde <mde@fleegix.org>
2012-01-11 15:26:43 -0800

    Command-line args should allow any order.

--- tests/parseargs.js
+++ tests/parseargs.js
@@ -30,87 +30,94 @@ var parseargs = require('../lib/parseargs')
       }
     ]
   , p = new parseargs.Parser(optsReg)
-  , z = function (s) { return s.split(' '); };
+  , z = function (s) { return s.split(' '); }
+  , res;
 
 // Long preemptive opt and val with equal-sign, ignore further opts
-p.parse(z('--tasks=foo --jakefile=asdf'));
-assert.equal('foo', p.opts.tasks);
-assert.equal(undefined, p.opts.jakefile);
+res = p.parse(z('--tasks=foo --jakefile=asdf'));
+assert.equal('foo', res.opts.tasks);
+assert.equal(undefined, res.opts.jakefile);
 
 // Long preemptive opt and val without equal-sign, ignore further opts
-p.parse(z('--tasks foo --jakefile=asdf'));
-assert.equal('foo', p.opts.tasks);
-assert.equal(undefined, p.opts.jakefile);
+res = p.parse(z('--tasks foo --jakefile=asdf'));
+assert.equal('foo', res.opts.tasks);
+assert.equal(undefined, res.opts.jakefile);
 
 // Long preemptive opt and no val, ignore further opts
-p.parse(z('--tasks --jakefile=asdf'));
-assert.equal(true, p.opts.tasks);
-assert.equal(undefined, p.opts.jakefile);
+res = p.parse(z('--tasks --jakefile=asdf'));
+assert.equal(true, res.opts.tasks);
+assert.equal(undefined, res.opts.jakefile);
 
 // Preemptive opt with no val, should be true
-p.parse(z('-T'));
-assert.equal(true, p.opts.tasks);
+res = p.parse(z('-T'));
+assert.equal(true, res.opts.tasks);
 
 // Preemptive opt with no val, should be true and ignore further opts
-p.parse(z('-T -f'));
-assert.equal(true, p.opts.tasks);
-assert.equal(undefined, p.opts.jakefile);
+res = p.parse(z('-T -f'));
+assert.equal(true, res.opts.tasks);
+assert.equal(undefined, res.opts.jakefile);
 
 // Preemptive opt with val, should be val
-p.parse(z('-T zoobie -f foo/bar/baz'));
-assert.equal('zoobie', p.opts.tasks);
-assert.equal(undefined, p.opts.jakefile);
+res = p.parse(z('-T zoobie -f foo/bar/baz'));
+assert.equal('zoobie', res.opts.tasks);
+assert.equal(undefined, res.opts.jakefile);
 
 // -f expects a value, -t does not (howdy is task-name)
-p.parse(z('-f zoobie -t howdy'));
-assert.equal('zoobie', p.opts.jakefile);
-assert.equal(true, p.opts.trace);
-assert.equal('howdy', p.taskNames[0]);
+res = p.parse(z('-f zoobie -t howdy'));
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(true, res.opts.trace);
+assert.equal('howdy', res.taskNames[0]);
+
+// Different order, -f expects a value, -t does not (howdy is task-name)
+res = p.parse(z('-f zoobie howdy -t'));
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(true, res.opts.trace);
+assert.equal('howdy', res.taskNames[0]);
 
 // -f expects a value, -t does not (foo=bar is env var)
-p.parse(z('-f zoobie -t foo=bar'));
-assert.equal('zoobie', p.opts.jakefile);
-assert.equal(true, p.opts.trace);
-assert.equal('bar', p.envVars.foo);
-assert.equal(undefined, p.taskName);
+res = p.parse(z('-f zoobie -t foo=bar'));
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(true, res.opts.trace);
+assert.equal('bar', res.envVars.foo);
+assert.equal(undefined, res.taskName);
 
 // -f expects a value, -t does not (foo=bar is env-var, task-name follows)
-p.parse(z('-f zoobie -t howdy foo=bar'));
-assert.equal('zoobie', p.opts.jakefile);
-assert.equal(true, p.opts.trace);
-assert.equal('bar', p.envVars.foo);
-assert.equal('howdy', p.taskNames[0]);
+res = p.parse(z('-f zoobie -t howdy foo=bar'));
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(true, res.opts.trace);
+assert.equal('bar', res.envVars.foo);
+assert.equal('howdy', res.taskNames[0]);
 
 // -t does not expect a value, -f does (throw howdy away)
-p.parse(z('-t howdy -f zoobie'));
-assert.equal(true, p.opts.trace);
-assert.equal('zoobie', p.opts.jakefile);
-assert.equal(undefined, p.taskName);
+res = p.parse(z('-t howdy -f zoobie'));
+assert.equal(true, res.opts.trace);
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(undefined, res.taskName);
 
 // --trace does not expect a value, -f does (throw howdy away)
-p.parse(z('--trace howdy --jakefile zoobie'));
-assert.equal(true, p.opts.trace);
-assert.equal('zoobie', p.opts.jakefile);
-assert.equal(undefined, p.taskName);
+res = p.parse(z('--trace howdy --jakefile zoobie'));
+assert.equal(true, res.opts.trace);
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(undefined, res.taskName);
 
 // --trace does not expect a value, -f does (throw howdy away)
-p.parse(z('--trace=howdy --jakefile=zoobie'));
-assert.equal(true, p.opts.trace);
-assert.equal('zoobie', p.opts.jakefile);
-assert.equal(undefined, p.taskName);
+res = p.parse(z('--trace=howdy --jakefile=zoobie'));
+assert.equal(true, res.opts.trace);
+assert.equal('zoobie', res.opts.jakefile);
+assert.equal(undefined, res.taskName);
 
 /*
 // Task-name with positional args
-p.parse(z('foo:bar[asdf,qwer]'));
+res = p.parse(z('foo:bar[asdf,qwer]'));
 assert.equal('asdf', p.taskArgs[0]);
 assert.equal('qwer', p.taskArgs[1]);
 
 // Opts, env vars, task-name with positional args
-p.parse(z('-f ./tests/Jakefile -t default[asdf,qwer] foo=bar'));
-assert.equal('./tests/Jakefile', p.opts.jakefile);
-assert.equal(true, p.opts.trace);
-assert.equal('bar', p.envVars.foo);
-assert.equal('default', p.taskName);
+res = p.parse(z('-f ./tests/Jakefile -t default[asdf,qwer] foo=bar'));
+assert.equal('./tests/Jakefile', res.opts.jakefile);
+assert.equal(true, res.opts.trace);
+assert.equal('bar', res.envVars.foo);
+assert.equal('default', res.taskName);
 assert.equal('asdf', p.taskArgs[0]);
 assert.equal('qwer', p.taskArgs[1]);
 */

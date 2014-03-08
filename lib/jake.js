jakejs/jake

commit b0fcf1cd80fff6dac7d9eecf4524eae66c0ca50c

Only throw if jakefile NAND jakelib

lib/jake.js
@@ -310,8 +310,10 @@ utils.mixin(jake, new (function () {
     else {
       opts = program.opts;
       // Load Jakefile and jakelibdir files
-      loader.loadFile(opts.jakefile);
-      loader.loadDirectory(opts.jakelibdir);
+      if(!loader.loadFile(opts.jakefile) && !loader.loadDirectory(opts.jakelibdir)) {
+        fail('No Jakefile. Specify a valid path with -f/--jakefile, ' +
+            'or place one in the current directory.');
+      }
 
       program.run();
     }

lib/loader.js
@@ -45,7 +45,8 @@ Loader = function () {
       // Dear God, why?
       , isCoffee = false
       // Warning, recursive
-      , exists;
+      , exists
+      , oldCwd = process.cwd();
 
     exists = function () {
       var cwd = process.cwd();
@@ -56,6 +57,8 @@ Loader = function () {
       if (!fileSpecified) {
         process.chdir("..");
         if (cwd === process.cwd()) {
+          // Restore the working directory on failure
+          process.chdir(oldCwd);
           return false;
         }
         return exists();
@@ -63,8 +66,7 @@ Loader = function () {
     };
 
     if (!exists()) {
-      fail('No Jakefile. Specify a valid path with -f/--jakefile, ' +
-          'or place one in the current directory.');
+      return false;
     }
 
     isCoffee = existsSync(jakefile + '.coffee');
@@ -72,6 +74,7 @@ Loader = function () {
       CoffeeScript = _requireCoffee();
     }
     require(utils.file.absolutize(jakefile));
+    return true;
   };
 
   this.loadDirectory = function (d) {
@@ -88,7 +91,10 @@ Loader = function () {
           require(path.join(dirname, filePath));
         }
       });
+      return true;
     }
+
+    return false;
   };
 };
 

commit d7f16ba26871e7fcfee4105c6162f19def44a405

Fixed regression introduced by b0fcf1c
That commit broke the ability to load both jakefile and jakelibdir.

lib/jake.js
@@ -310,7 +310,10 @@ utils.mixin(jake, new (function () {
     else {
       opts = program.opts;
       // Load Jakefile and jakelibdir files
-      if(!loader.loadFile(opts.jakefile) && !loader.loadDirectory(opts.jakelibdir)) {
+      var jakefileLoaded = loader.loadFile(opts.jakefile);
+      var jakelibdirLoaded = loader.loadDirectory(opts.jakelibdir);
+
+      if(!jakefileLoaded && !jakelibdirLoaded) {
         fail('No Jakefile. Specify a valid path with -f/--jakefile, ' +
             'or place one in the current directory.');
       }
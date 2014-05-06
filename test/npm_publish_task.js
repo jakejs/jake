d4ea433b18e8eaf55f7dae0e4e015d9cd80576e9
mde <mde@fleegix.org>
2014-05-06 12:02:14 -0700

    Get tests passing for publish

--- test/npm_publish_task.js
+++ test/npm_publish_task.js
@@ -14,6 +14,18 @@ var tests = {
 
 , 'test default task': function (next) {
     h.exec('../bin/cli.js  -f Jakefile.publish publish', function (out) {
+      var expected = [
+            'Fetched remote tags.'
+          , 'On branch v0.0'
+          , 'Bumped version number to v0.0.2.'
+          , 'Created package for zerb v0.0.2'
+          , 'Publishing zerb v0.0.2'
+          , 'pkg/zerb-v0.0.2.tar.gz'
+          , 'Published to NPM'
+          , 'Cleaned up package'
+          ].join('\n');
+      assert.equal(expected, out);
+      next();
     });
   }
 

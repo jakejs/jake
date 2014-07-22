400eecc3f37a897c488ebdc622dd51a2eaef41fc
Arno Velds <a.velds@nki.nl>
2014-07-22 11:28:47 +0200

    Rename from concurrent to parallel

--- test/concurrent.js
+++ test/concurrent.js
@@ -11,38 +11,38 @@ var tests = {
     process.chdir('../');
   }
 
-, 'test simple concurrent task 1': function (next) {
-    h.exec('../bin/cli.js concurrent:simple1', function (out) {
+, 'test simple parallel prerequisites 1': function (next) {
+    h.exec('../bin/cli.js parallel:simple1', function (out) {
       assert.equal('Started A\nStarted B\nFinished B\nFinished A', out);
       next();
     });
   }
-, 'test simple concurrent task 2': function (next) {
-    h.exec('../bin/cli.js concurrent:simple2', function (out) {
+, 'test simple parallel prerequisites 2': function (next) {
+    h.exec('../bin/cli.js parallel:simple2', function (out) {
       assert.equal('Started C\nStarted D\nFinished C\nFinished D', out);
       next();
     });
   }
-, 'test sequential concurrent tasks': function (next) {
-    h.exec('../bin/cli.js concurrent:seqconcurrent', function (out) {
+, 'test sequential parallel prerequisites': function (next) {
+    h.exec('../bin/cli.js parallel:seqparallel', function (out) {
       assert.equal('Started A\nStarted B\nFinished B\nFinished A\nStarted C\nStarted D\nFinished C\nFinished D', out);
       next();
     });
   }
-, 'test concurrent concurrent tasks': function (next) {
-    h.exec('../bin/cli.js concurrent:concurrentconcurrent', function (out) {
+, 'test parallel parallel prerequisites': function (next) {
+    h.exec('../bin/cli.js parallel:parallelparallel', function (out) {
       assert.equal('Started A\nStarted B\nStarted C\nStarted D\nFinished B\nFinished C\nFinished A\nFinished D', out);
       next();
     });
   }
-, 'test concurrent task with subdependency': function (next) {
-    h.exec('../bin/cli.js concurrent:subdep', function (out) {
+, 'test parallel prerequisites with subdependency': function (next) {
+    h.exec('../bin/cli.js parallel:subdep', function (out) {
       assert.equal('Started A\nFinished A\nStarted Ba\nFinished Ba', out);
       next();
     });
   }
-, 'test failing in concurrent tasks': function (next) {
-    h.exec('../bin/cli.js concurrent:Cfail', {breakOnError:false},function (out) {
+, 'test failing in parallel prerequisites': function (next) {
+    h.exec('../bin/cli.js parallel:Cfail', {breakOnError:false},function (out) {
       assert.equal(1, out.code);
       next();
     });

f069434881f93dbacbacdad738dd047426c57b27
Igor Savin <iselwin@gmail.com>
2019-07-28 14:52:07 +0200

    Replace JSHint with ESLint + prettier

--- test/helpers.js
+++ test/helpers.js
@@ -1,80 +1,75 @@
 var exec = require('child_process').exec;
 
-var helpers = new (function () {
-  var _tests
-    , _names = []
-    , _name
-    , _callback
-    , _runner = function () {
-        if (!!(_name = _names.shift())) {
-          console.log('Running ' + _name);
-          _tests[_name]();
-        }
-        else {
-          _callback();
-        }
-      };
+var helpers = new (function() {
+  var _tests,
+    _names = [],
+    _name,
+    _callback,
+    _runner = function() {
+      if (!!(_name = _names.shift())) {
+        console.log('Running ' + _name);
+        _tests[_name]();
+      } else {
+        _callback();
+      }
+    };
 
-  this.exec = function () {
-    var args = Array.prototype.slice.call(arguments)
-      , arg
-      , cmd = args.shift()
-      , opts = {}
-      , callback;
+  this.exec = function() {
+    var args = Array.prototype.slice.call(arguments),
+      arg,
+      cmd = args.shift(),
+      opts = {},
+      callback;
     // Optional opts/callback or callback/opts
     while ((arg = args.shift())) {
       if (typeof arg == 'function') {
         callback = arg;
-      }
-      else {
+      } else {
         opts = arg;
       }
     }
 
     cmd += ' --trace';
     var execOpts = opts.execOpts ? opts.execOpts : {};
-    exec(cmd, execOpts, function (err, stdout, stderr) {
+    exec(cmd, execOpts, function(err, stdout, stderr) {
       var out = helpers.trim(stdout);
       if (err) {
         if (opts.breakOnError === false) {
           return callback(err);
-        }
-        else {
+        } else {
           throw err;
         }
       }
       if (stderr) {
         callback(stderr);
-      }
-      else {
+      } else {
         callback(out);
       }
     });
   };
 
-  this.trim = function (s) {
+  this.trim = function(s) {
     var str = s || '';
     return str.replace(/^\s*|\s*$/g, '');
   };
 
-  this.parse = function (s) {
+  this.parse = function(s) {
     var str = s || '';
     str = helpers.trim(str);
     str = str.replace(/'/g, '"');
     return JSON.parse(str);
   };
 
-  this.run = function (tests, callback) {
+  this.run = function(tests, callback) {
     _tests = tests;
     _names = Object.keys(tests);
     _callback = callback;
     _runner();
   };
 
-  this.next = function () {
+  this.next = function() {
     _runner();
   };
-
 })();
 
 module.exports = helpers;
--- test/selfdep.js
+++ test/selfdep.js
@@ -1,27 +1,25 @@
-var assert = require('assert')
-  , h = require('./helpers');
+var assert = require('assert'),
+  h = require('./helpers');
 
 var tests = {
-
-  'before': function () {
+  before: function() {
     process.chdir('./test');
-  }
+  },
 
-, 'after': function () {
+  after: function() {
     process.chdir('../');
-  }
-, 'test selfdepconst': function (next) {
-    h.exec('../bin/cli.js selfdepconst', {breakOnError:false}, function (out) {
+  },
+  'test selfdepconst': function(next) {
+    h.exec('../bin/cli.js selfdepconst', { breakOnError: false }, function(out) {
+      assert.equal(1, out.code);
+      next();
+    });
+  },
+  'test selfdepdyn': function(next) {
+    h.exec('../bin/cli.js selfdepdyn', { breakOnError: false }, function(out) {
       assert.equal(1, out.code);
       next();
     });
   }
-, 'test selfdepdyn': function (next) {
-    h.exec('../bin/cli.js selfdepdyn', {breakOnError:false}, function (out) {
-      assert.equal(1, out.code);
-      next();
-    });
-  }
-}
+};
 module.exports = tests;
-

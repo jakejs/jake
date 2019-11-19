e85d64a9ca58abcfa4f22983bf07996760bab655
mde <mde@fleegix.org>
2019-11-18 22:19:19 -0800

    Updated linting rules

--- .eslint.js
+++ .eslint.js
@@ -1,32 +1,34 @@
-{
-  "env": {
-    "node": true,
-    "es6": true
+
+module.exports = {
+  env: {
+    node: true,
+    es6: true
   },
-  "globals": {
-    "jake": "readable",
-    "task": "readable",
-    "namespace": "readable",
-    "desc": "readable",
-    "complete": "readable",
-    "file": "readable",
-    "directory": "readable",
-    "fail": "readable"
+  globals: {
+    jake: 'readable',
+    task: 'readable',
+    namespace: 'readable',
+    desc: 'readable',
+    complete: 'readable',
+    file: 'readable',
+    directory: 'readable',
+    fail: 'readable'
   },
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  "extends": "eslint:recommended",
-  "rules": {
-    "no-console": "off",
-    "no-empty": "off",
-    "no-unused-vars": ["error", { "args": "none" }],
-    "no-useless-escape": "off",
-    "space-before-function-paren": ["error", {
-      "anonymous": "always",
-      "named": "never",
-      "asyncArrow": "always"
+  extends: 'eslint:recommended',
+  rules: {
+    'indent': ['error', 2],
+    'no-console': 'off',
+    'no-empty': 'off',
+    'no-unused-vars': ['error', { 'args': 'none' }],
+    'no-useless-escape': 'off',
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'never',
+      'asyncArrow': 'always'
     }]
   }
 }
